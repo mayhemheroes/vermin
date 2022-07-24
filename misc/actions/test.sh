7a325fd86b171bea247aa9f0c497e1b805831f3f
Morten Kristensen <me@mortens.dev>
2022-07-24 16:12:09 +0200

    [actions] Don't use Python 2.7

--- misc/actions/test.sh
+++ misc/actions/test.sh
@@ -3,7 +3,7 @@ set -x
 
 PYTHON_VERSION=$(python -c "import sys;v=sys.version_info;print('{}.{}'.format(v[0],v[1]))")
 
-if [[ $PYTHON_VERSION = 2.7 || $PYTHON_VERSION = 3.4 || $PYTHON_VERSION = 3.5 ]]; then
+if [[ $PYTHON_VERSION = 3.4 || $PYTHON_VERSION = 3.5 ]]; then
   make test
 else
   setup() {
